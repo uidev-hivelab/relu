ee97aff29b160ddbcd55cf7903686ed1cfb344b3
Quy_뀌 <quylv@hivelab.com>
2024-10-11 16:59:39 +0700

    setting

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+// import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById('root')!).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>,
+)
